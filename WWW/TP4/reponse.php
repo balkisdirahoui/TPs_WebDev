e4ae2be2baf9aaf8df3bf400f850249a061ab8ce
Balkis <60971348+balkisdirahoui@users.noreply.github.com>
2021-06-04 15:59:57 +0100

    Balkis Part

--- WWW/TP4/reponse.php
+++ WWW/TP4/reponse.php
@@ -14,7 +14,7 @@
 
 	
 	<div align="center">
-		<table border=3px bordercolor="orange" bgcolor="white" width=800px margin=0px  cellspacing=0px >	
+		<table border=3px bordercolor="orange" bgcolor="white" width=800px margin=10px  cellspacing=10px >	
 		  <tr>	
 		   	
 			<td style="border:none;">
@@ -39,6 +39,90 @@
 						}
 						echo $_POST['nom'];
 						?>
+					</p>
+						
+
+		    </td>
+	       </tr>
+		</table>
+		<br>
+		<br>
+		<!--partie affichage Informations-->
+				<table border=3px bordercolor="red" bgcolor="white" width=800px margin=10px  cellspacing=10px >	
+		  <tr>	
+		   	
+			<td style="border:none;">
+			 <h1 align="center"><font face="Arial" color="Blue">&Egrave;tat Global </font></h1>
+
+					<p align="left">
+						
+						<b>Nom</b> :  <?php echo $_POST['nom'];?> <br><br>
+						 
+            
+           
+                        <b>Pr&eacute;nom</b> : <?php echo $_POST['prenom'];?><br><br>
+
+                        <b>Date de naissance</b> :  <?php echo $_POST['dateN'];?><br><br>
+                        <b>N° de téléphone </b> : <?php echo $_POST['tel'];?><br><br>
+
+                        <b>Genre</b> : <?php echo $_POST['genre'];?><br><br>
+                        <b>Adresse</b> : <?php echo $_POST['adresse'];?><br><br>
+                        <b>Civilit&eacute;</b> : <?php echo $_POST['civilité']; ?><br><br>
+                        <b>(*) Sp&eacute;cialit&eacute;s</b> : <?php 
+					   
+						if(!empty($_POST['choix'])) 
+						{
+							
+							$choix = $_POST['choix'];
+							$length = count($_POST['choix']);
+							foreach ($choix as $ch)
+							{ 
+								//si il ne selectione pas
+								if(strcmp($ch, "")==0){
+									echo "la mention : Demande de Recours.";
+								}
+								else{
+									//si c'est le dernier element
+								if(strcmp($ch,$_POST['choix'][$length-1])==0){
+								echo $ch;	
+								}
+								else{
+                                echo $ch."-";
+								}
+								}
+							
+							}					
+						}else{
+							echo "la mention : Demande de Recours.";
+						}
+						
+						
+						
+						?><br><br>
+                        <b>Langues souhait&eacute;es &agrave; l'universit&eacute;</b> : 
+						<?php
+							
+
+						if(!empty($_POST['Langue'])) 
+						{
+							
+							$langues = $_POST['Langue'];
+							$length = count($_POST['Langue']);
+							foreach ($langues as $langue){ 
+							//si c'est le dernier element
+								if(strcmp($langue,$_POST['Langue'][$length-1])==0){
+								echo $langue;	
+								}
+								else{
+                                echo $langue."-";
+								}
+							}					}
+						
+						?>
+						
+						<br><br>
+                       
+						
 					</p>
 						
 
